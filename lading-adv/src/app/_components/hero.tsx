705674c4df3b4a3ba051e67b35e9b82853836ed5
manoel ferreira <95552207+ManelFer@users.noreply.github.com>
2025-02-15 22:19:43 -0300

    finalizado
    terminei a primeira parte. Iniciando o about-me

--- lading-adv/src/app/_components/hero.tsx
+++ lading-adv/src/app/_components/hero.tsx
@@ -1,3 +1,4 @@
+import {Envelope} from '@phosphor-icons/react/dist/ssr'
 import Image from "next/image";
 import fundo from '../../../public/hero.jpeg'; // Certifique-se de que o caminho da imagem está correto
 
@@ -16,13 +17,21 @@ export function Hero() {
             <div className='container mx-auto pt-16 pb-16 md:pb-0 px-4 relative'>{/* Contêiner para o texto */}
                 <article> {/* Grid com uma coluna para telas pequenas e duas para telas médias */}
                     <div className="space-y-6 text-right"> {/* Texto branco */}
-                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bol leading-10"> Sarah Priscila </h1>
-                        <h2 className="text-lg md:text-xl text-gray-300 leading-7"> Advogada especialis </h2>
-                        <p className="text-base md:text-lg text-gray-400 whitespace-pre-line">Especializada em litígios e resolução de conflitos,<br/>
+                        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-10"> Sarah Priscila </h1>
+                        <h2 className="text-lg md:text-xl text-gray-300 leading-7"> Advogada especialista </h2>
+                        <p className="text-base md:text-lg text-gray-400 whitespace-pre-line mb-4"> {/* Ajuste a margem inferior aqui */}
+                            Especializada em litígios e resolução de conflitos,<br/>
                             minha abordagem estratégica garante que seus interesses sejam sempre priorizados.
                             <br />  
                             Vamos trabalhar juntos para alcançar seus objetivos.
                         </p>
+                        <a href="#" className='flex justify-end'> {/* Alinhar o botão à direita */}
+                            
+                            <button className="bg-blue-500 hover:scale-110 duration-300 hover:bg-blue-900 text-white py-2 px-4 rounded mt-4 flex items-center"><Envelope className='w-5 h-5'/> {/* Adicionei mt-4 aqui */}
+                                Contato
+                            </button>
+                            
+                        </a>
                     </div>
                 </article>
             </div>
